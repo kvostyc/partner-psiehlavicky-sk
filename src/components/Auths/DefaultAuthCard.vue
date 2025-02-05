435779e3df27fb40837e90112efea101be9278d4
kvostyc <you@example.com>
2025-02-05 19:53:05 +0100

    Changes

--- src/components/Auths/DefaultAuthCard.vue
+++ src/components/Auths/DefaultAuthCard.vue
@@ -8,22 +8,6 @@ const props = defineProps(['subtitle', 'title', 'customClasses'])
     class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
   >
     <div class="flex flex-wrap items-center">
-      <div class="hidden w-full xl:block xl:w-1/2">
-        <div class="py-17.5 px-26 text-center">
-          <router-link class="mb-5.5 inline-block" to="/">
-            <img class="hidden dark:block" src="@/assets/images/logo/logo.svg" alt="Logo" />
-            <img class="dark:hidden" src="@/assets/images/logo/logo-dark.svg" alt="Logo" />
-          </router-link>
-
-          <p class="font-medium 2xl:px-20">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.
-          </p>
-
-          <span class="mt-15 inline-block">
-            <img src="@/assets/images/illustration/illustration-03.svg" alt="illustration" />
-          </span>
-        </div>
-      </div>
       <div class="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
         <div class="w-full p-4 sm:p-12.5 xl:p-17.5">
           <span class="mb-1.5 block font-medium">{{ props.subtitle }}</span>
